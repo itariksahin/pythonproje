244f53d2a0e24ddf119f09fb428fb2e485927459
itariksahin <itariksahin@hotmail.com>
2022-04-28 21:55:39 +0300

    Update oyuncu.py

--- oyuncu/oyuncu.py
+++ oyuncu/oyuncu.py
@@ -1,44 +1,47 @@
+#Video üzerinde secim yapmak için opencv denemesidir
+#28.04.2022
+#İki futbol takımı oyuncularının secimi amaclanmıştır bu nedenle 
+# kırmızı ve mavi formalı iki takım tercih edilmiştir
 from getopt import gnu_getopt
 import cv2
 import os
 import numpy as np
 
-#Reading the video
+#video alınıyor
 vidcap = cv2.VideoCapture('mac.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
 idx = 0
 
-#Read the video frame by frame
+#Video fotograflara ayırlıyor
 while success:
-	#converting into hsv image
+	
 	hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-	#green range
+	#yeşil renk için aralık belirleniyor
 	lower_green = np.array([40,40, 40])
 	upper_green = np.array([70, 255, 255])
-	#blue range
+	#Mavi renk için aralık belirleniyor
 	lower_blue = np.array([110,50,50])
 	upper_blue = np.array([130,255,255])
 
-	#Red range
+	#Kırmızı renk için aralık belirleniyor
 	lower_red = np.array([0,31,255])
 	upper_red = np.array([176,255,255])
 
-	#white range
+	#Beyaz renk için aralık belirleniyor
 	lower_white = np.array([0,0,0])
 	upper_white = np.array([0,0,255])
 
-    #Define a mask ranging from lower to uppper
+    #Azdan çok renge bir maske tanımlama
 	mask = cv2.inRange(hsv, lower_green, upper_green)
-    #Do masking
+    #maskeleme
 	res = cv2.bitwise_and(image, image, mask=mask)
-	#convert to hsv to gray
+	#fotoraflar griye dönüştürülüyor
 	res_bgr = cv2.cvtColor(res,cv2.COLOR_HSV2BGR)
 	res_gray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
 
-    #Defining a kernel to do morphological operation in threshold image to 
-    #get better output.
+   
 	kernel = np.ones((13,13),np.uint8)
 	thresh = cv2.threshold(res_gray,127,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 	thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
@@ -54,19 +57,19 @@ while success:
 	for c in contours:
 		x,y,w,h = cv2.boundingRect(c)
 		
-		#Detect players
+		#Futbolcular belirleniyor
 		if(h>=(1.5)*w):
 			if(w>15 and h>= 15):
 				idx = idx+1
 				player_img = image[y:y+h,x:x+w]
 				player_hsv = cv2.cvtColor(player_img,cv2.COLOR_BGR2HSV)
-				#If player has blue jersy
+				#Mavi Ağırlık formalıları secelim
 				mask1 = cv2.inRange(player_hsv, lower_blue, upper_blue)
 				res1 = cv2.bitwise_and(player_img, player_img, mask=mask1)
 				res1 = cv2.cvtColor(res1,cv2.COLOR_HSV2BGR)
 				res1 = cv2.cvtColor(res1,cv2.COLOR_BGR2GRAY)
 				nzCount = cv2.countNonZero(res1)
-				#If player has red jersy
+				#kırmızı Ağırlık formalıları secelim
 				mask2 = cv2.inRange(player_hsv, lower_red, upper_red)
 				res2 = cv2.bitwise_and(player_img, player_img, mask=mask2)
 				res2 = cv2.cvtColor(res2,cv2.COLOR_HSV2BGR)
@@ -74,14 +77,12 @@ while success:
 				nzCountred = cv2.countNonZero(res2)
 
 				if(nzCount >= 20):
-					#Mark blue jersy players as france
-					cv2.putText(image, 'France', (x-2, y-2), font, 0.8, (255,0,0), 2, cv2.LINE_AA)
+					cv2.putText(image, 'KIRMIZI TAKIM', (x-2, y-2), font, 0.8, (255,0,0), 2, cv2.LINE_AA)
 					cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),3)
 				else:
 					pass
 				if(nzCountred>=20):
-					#Mark red jersy players as belgium
-					cv2.putText(image, 'Belgium', (x-2, y-2), font, 0.8, (0,0,255), 2, cv2.LINE_AA)
+					cv2.putText(image, 'MAVİ TAKIM', (x-2, y-2), font, 0.8, (0,0,255), 2, cv2.LINE_AA)
 					cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),3)
 				else:
 					pass
@@ -89,7 +90,7 @@ while success:
 			player_img = image[y:y+h,x:x+w]
 		
 			player_hsv = cv2.cvtColor(player_img,cv2.COLOR_BGR2HSV)
-			#white ball  detection
+			#TOPUN SECİLMESİ
 			mask1 = cv2.inRange(player_hsv, lower_white, upper_white)
 			res1 = cv2.bitwise_and(player_img, player_img, mask=mask1)
 			res1 = cv2.cvtColor(res1,cv2.COLOR_HSV2BGR)
@@ -98,13 +99,12 @@ while success:
 	
 
 			if(nzCount >= 3):
-				# detect football
-				cv2.putText(image, 'football', (x-2, y-2), font, 0.8, (0,255,0), 2, cv2.LINE_AA)
+				cv2.putText(image, 'TOP', (x-2, y-2), font, 0.8, (0,255,0), 2, cv2.LINE_AA)
 				cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
 
 
 	cv2.imwrite("./Cropped/frame%d.jpg" % count, res)
-	print ('Read a new frame: '), success     # save frame as JPEG file	
+	print ('Yeni cerceve: '), success   
 	count += 1
 	cv2.imshow('Match Detection',image)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
